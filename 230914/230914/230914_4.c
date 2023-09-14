a8e601b5dc79f869bea946201e2e97bcc4aecd14
nanakyeong <dlskrud468@naver.com>
2023-09-14 15:24:26 +0900

    year

---
+++ 230914/230914/230914_4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    int year;
+    scanf_s("%d", &year);
+
+    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+        printf("¿±≥‚¿‘¥œ¥Ÿ.");
+    }
+    else {
+        printf("¿±≥‚¿Ã æ∆¥’¥œ¥Ÿ.");
+    }
+
+    return 0;
+}
