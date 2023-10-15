a0bdd5d2e110453d0296c35b8e85096a2fbbe2ef
nanakyeong <dlskrud468@naver.com>
2023-10-15 21:42:59 +0900

    Monte Carlo algorithm

--- 230921/230921/lecture_4.c
+++ 230921/230921/lecture_4.c
@@ -1,60 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
-int main() {
+int main(void) {
 
-    int i;
-    int pro = 0;
-    int newPro;
-    int squares;
-    int space;
-    double x = 0;
-    double y = 0;
-    double circle = 0;
-    double pi = 0;
-    double final;
-    long long count = 0;
+	double x = 0, y = 0, circle = 0, count = 0, pi;
+	int i, j, space, square, newPro, pro = 0;
 
-    srand(time(NULL));
+	srand(time(NULL));
 
-    for (i = 0; i < 1000000000; i++) {
+	for (i = 0; i < 1000000000; i++) {
+		x = (double)rand() / (double)RAND_MAX * 2 - 1;
+		y = (double)rand() / (double)RAND_MAX * 2 - 1;
 
-        x = (double)rand() / (double)RAND_MAX * 2 - 1;
-        y = (double)rand() / (double)RAND_MAX * 2 - 1;
+		count++;
 
-        count++;
+		if ((x * x) + (y * y) <= 1) {
+			circle++;
+		}
 
-        if ((x * x) + (y * y) <= 1) {
-            circle++;
-        }
+		pi = 4 * (circle / (double)count);
 
-        pi = (circle / (double)count) * 4;
+		newPro = (count * 100) / 1000000000;
 
-        newPro = (count * 100) / 1000000000;
+		if (newPro > pro) {
 
-        if (newPro > pro) {
+			pro = newPro;
 
-            pro = newPro;
+			printf("%d%% 진행.. 원주율: %.7f", pro, pi);
 
-            printf("%d%% 진행.. 원주율: %.7f ", pro, pi);
+			square = pro / 5;
+			for (j = 0; j < square; j++) {
+				printf("■");
+			}
 
-            squares = pro / 5;
-            for (int j = 0; j < squares; j++) {
-                printf("■");
-            }
+			space = 20 - square;
+			for (j = 0; j < space; j++) {
+				printf("□");
+			}
 
-            space = 20 - squares;
-            for (int j = 0; j < space; j++) {
-                printf("□");
-            }
+			printf("\n");
+		}
+	}
 
-            printf("\n");
-        }
-    }
+	double final = 4 * (circle / (double)count);
+	printf("원주율 : %.7f", final);
 
-    final = (circle / (double)count) * 4;
-    printf("원주율: %.7f\n", final);
+	return 0;
 
-    return 0;
+
 }
