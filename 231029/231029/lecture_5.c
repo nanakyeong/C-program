11b9713f35557ceb566331ad6e72b96c2f80a780
nanakyeong <dlskrud468@naver.com>
2023-10-19 23:47:54 +0900

    end of file

--- 231029/231029/lecture_5.c
+++ 231029/231029/lecture_5.c
@@ -2,10 +2,13 @@
 
 int main() {
     FILE* fp = NULL;
-    fopen_s(&fp. "data.txt", "rt");
+    fopen_s(&fp, "data.txt", "rt");
 
     int n1, n2, n3;
     while (fscanf_s(fp, "%d %d %d", &n1, &n2, &n3) != EOF) {
-        printf("n1 = %d, n2 = ")
+        printf("n1 = %d, n2 = %d ,n3 = %d",n1, n2, n3);
     }
+
+    fclose(fp);
+    return 0;
 }
